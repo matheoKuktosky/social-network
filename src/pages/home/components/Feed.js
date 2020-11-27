6ea9626ecb30b50ed63e6a655cc07c6e06a68b84
Matheo <kuktoskymatheo@gmail.com>
2020-11-27 01:14:38 -0300

    Feed

--- src/pages/home/components/Feed.js
+++ src/pages/home/components/Feed.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import PostCard from '../../../components/PostCard'
+
+const useStyles = makeStyles(theme =>({
+    root: {
+
+    }
+}));
+
+const posts = [
+    { id: 1, 
+      author: {
+          id: 1,
+          name: 'Oleg Korolenko',
+          username: 'olegkorolenko',
+          avatar: '/images/avatars/avatar_1.jpeg'
+      },
+      title: 'To my friend Ozob',
+      date: 'April 7, 2020',
+      description: 'Lets share a bottle of Krakovia!',
+      hashtags: '#putin, #krakovia, #patati&patata',
+      image: '/images/posts/post1.jpeg'
+    },
+    { id: 2, 
+        author: {
+            id: 1,
+            name: 'Oleg Korolenko',
+            username: 'olegkorolenko',
+            avatar: '/images/avatars/avatar_1.jpeg'
+        },
+        title: 'Im lost, help me!',
+        date: 'April 8, 2020',
+        description: 'That damn Krakovia hit me hard! Now I´m even seeing Ozob inside a game!',
+        hashtags: '#hangover, #krakovia, #Cyberpunk2077',
+        image: '/images/posts/post2.jpeg'
+    }
+];
 
 function Feed(){
+    const classes = useStyles();
+
     return(
-        <div>
-            Feed
+        <div className={classes.root}>
+            {
+                posts.map(post => (<PostCard key={post.id} post={post} />))
+            }            
         </div>
     )
 }
--- src/pages/home/components/Navbar.js
+++ src/pages/home/components/Navbar.js
@@ -11,7 +11,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 const useStyles = makeStyles(theme => ({
     root: {
         padding: theme.spacing(2), // 8*2
-        width: 275
+        width: 275,
+        marginRight: theme.spacing(2)
     },
     button: {
         width: '100%'
